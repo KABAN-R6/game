a31ea9818839f16b69b0a01822a241e036bae4e7
is13126 <is13126@B206-2>
2023-04-03 10:32:13 +0700

    afsfaswrtfa

--- game/MainWindow.xaml.cs
+++ game/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace game
 {
@@ -23,49 +12,59 @@ namespace game
     {
         List<player> players;
         List<questions> quuestions;
+        string a;
+        int v;
+        
         public MainWindow()
         {
             InitializeComponent();
-            players= new List<player>()
+            players = new List<player>()
             {
-                new player("1", 10),
-                new player("2", 0),
-                new player("3", 0),
+                new player("Костя", 10),
+                new player("Данил", 0),
+                new player("Валера", 0),
 
             };
             Update();
             quuestions = new List<questions>()
             {
-                new questions("qfoqfepok[fe[qfe","1", 100),
-                
+                new questions("Что такое этикет?","правила поведения", 100),
+                new questions("Как переводится с французского языка слово «этикет»?","ярлык", 200),
+                new questions("Что значит слово «вежливость»?","быть вежливым", 300),
+                new questions("Кто должен пройти первым: входящий в магазин или выходящий в это же время из него?","входящий должен пропустить выходящего", 400),
+                new questions("Какие существуют виды этикета?","Деловой, дипломатический, военный, педагогический, врачебный, поведениев общественных местах", 500),
 
             };
-           
+
 
         }
 
         private void list1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
+
         }
         public void Update()
         {
-            
+
             players = players.ToList();
             list2.ItemsSource = players;
-            
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
 
-            if (tx2.Text == "" || tx2.Text == null)
+            if (tx2.Text == null || tx1.Text == "")
             {
-
+               
             }
-            players.Add(new player(tx2.Text, 0));
-            Update();
+            else
+            {
+                players.Add(new player(tx2.Text, 0));
+                Update();
+            }
+            
 
 
         }
@@ -77,56 +76,68 @@ namespace game
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           b1.Visibility = Visibility.Collapsed;
-            vpr.Text = "123123";
-           
+            b1.Visibility = Visibility.Collapsed;
+            vpr.Text = quuestions[0].question;
+            a = quuestions[0].answer;
+            v = quuestions[0].Point;
         }
 
         private void b2_Click(object sender, RoutedEventArgs e)
         {
             b2.Visibility = Visibility.Collapsed;
-            vpr.Text = "123123";
+            vpr.Text = quuestions[1].question;
+            a = quuestions[1].answer;
+            v = quuestions[1].Point;
         }
 
         private void b3_Click(object sender, RoutedEventArgs e)
         {
             b3.Visibility = Visibility.Collapsed;
-            vpr.Text = "123123";
+            vpr.Text = quuestions[2].question;
+            a = quuestions[2].answer;
+            v = quuestions[2].Point;
         }
 
         private void b4_Click(object sender, RoutedEventArgs e)
         {
             b4.Visibility = Visibility.Collapsed;
-            vpr.Text = "123123";
+            vpr.Text = quuestions[3].question;
+            a = quuestions[3].answer;
+            v = quuestions[3].Point;
         }
 
         private void b5_Click(object sender, RoutedEventArgs e)
         {
             b5.Visibility = Visibility.Collapsed;
-            vpr.Text = "123123";
+            vpr.Text = quuestions[4].question;
+            a = quuestions[4].answer;
+            v = quuestions[4].Point;
         }
 
         private void vpr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            if (otv.Text == "1")
+            if (otv.Text == a)
             {
                 if (list2.SelectedItem == null)
                 {
                     MessageBox.Show("Выберети пользователя");
                 }
-               
+                int d = 0;
+                int c = v;
                 var b = players.FindIndex(u => u == list2.SelectedItem);
-                players[b].Point = 100;
-
+                players[b].Point = d+c  ;
+                d=d + c;
                 Update();
 
 
             }
+            vpr.Text = "";
+            otv.Text = "";
             Update();
         }
     }
