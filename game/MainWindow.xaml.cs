625059c5142baa0bc2ea132fff0dca17b4ab95ef
is13126 <is13126@B206-2>
2023-04-03 12:05:56 +0700

    asfasfasf

--- game/MainWindow.xaml.cs
+++ game/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace game
             InitializeComponent();
             players = new List<player>()
             {
-                new player("Костя", 10),
+                new player("Костя", 0),
                 new player("Данил", 0),
                 new player("Валера", 0),
 
@@ -33,9 +33,9 @@ namespace game
                 new questions("Что значит слово «вежливость»?","быть вежливым", 300),
                 new questions("Кто должен пройти первым: входящий в магазин или выходящий в это же время из него?","входящий должен пропустить выходящего", 400),
                 new questions("Какие существуют виды этикета?","Деловой, дипломатический, военный, педагогический, врачебный, поведениев общественных местах", 500),
-
+                
             };
-
+            tx.ItemsSource = players;
 
         }
 
@@ -55,16 +55,18 @@ namespace game
         {
 
 
-            if (tx2.Text == null || tx1.Text == "")
+            if (tx2.Text == null || tx2.Text == "")
             {
-               
+                MessageBox.Show("21312");
             }
             else
             {
                 players.Add(new player(tx2.Text, 0));
                 Update();
             }
+                
             
+           
 
 
         }
@@ -87,7 +89,8 @@ namespace game
             b2.Visibility = Visibility.Collapsed;
             vpr.Text = quuestions[1].question;
             a = quuestions[1].answer;
-            v = quuestions[1].Point;
+            v = quuestions[1].Point *2;
+            MessageBox.Show("X2");
         }
 
         private void b3_Click(object sender, RoutedEventArgs e)
@@ -126,19 +129,46 @@ namespace game
                 if (list2.SelectedItem == null)
                 {
                     MessageBox.Show("Выберети пользователя");
+                    
                 }
-                int d = 0;
-                int c = v;
-                var b = players.FindIndex(u => u == list2.SelectedItem);
-                players[b].Point = d+c  ;
-                d=d + c;
-                Update();
-
+                
+                    int d = 0;
+                    int c = v;
+                    var b = players.FindIndex(u => u == list2.SelectedItem);
+                    players[b].Point = players[b].Point + c;
+                    
+                    Update();
+                
+                
+                
 
             }
             vpr.Text = "";
             otv.Text = "";
             Update();
+
+             
+            
+            
+        
+        }
+
+        private void tx2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+
+
+
+        }
+
+        private void tx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
